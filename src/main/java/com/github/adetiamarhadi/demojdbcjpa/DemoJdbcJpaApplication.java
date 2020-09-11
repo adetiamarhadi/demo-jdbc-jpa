af1dffe8547584abedaffd4ed0352b286a00d907
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-11 11:27:36 +0700

    Execute the findAll method using CommandLineRunner

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/DemoJdbcJpaApplication.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/DemoJdbcJpaApplication.java
@@ -1,13 +1,25 @@
 package com.github.adetiamarhadi.demojdbcjpa;
 
+import com.github.adetiamarhadi.demojdbcjpa.jdbc.PersonJdbcDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class DemoJdbcJpaApplication {
+public class DemoJdbcJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	PersonJdbcDao personJdbcDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJdbcJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("{}", this.personJdbcDao.findAll());
+	}
 }
