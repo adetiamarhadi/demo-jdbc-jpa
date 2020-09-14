b6130ecd5b8a3fb3ccc29e9797b6a625ae5b8b78
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-14 09:51:59 +0700

    defining person entity

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Person.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Person.java
@@ -5,13 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String location;
