a64cef9a999aabd2dacd6fedb4daad18dcbf68eb
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-16 13:10:59 +0700

    writing unit test for findById method

---
+++ src/test/java/com/github/adetiamarhadi/demojdbcjpa/jpa/CourseRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.github.adetiamarhadi.demojdbcjpa.jpa;
+
+import com.github.adetiamarhadi.demojdbcjpa.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Test
+    void findById() {
+        Course course = this.courseRepository.findById(1004L);
+        assertEquals("Udemy - Hibernate", course.getName());
+    }
+}
