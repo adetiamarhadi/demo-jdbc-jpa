63c1d4cea2167b1d3c3438569c5e76547e4c5007
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-16 10:43:54 +0700

    create jpa entity course

---
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Course.java
@@ -0,0 +1,24 @@
+package com.github.adetiamarhadi.demojdbcjpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
