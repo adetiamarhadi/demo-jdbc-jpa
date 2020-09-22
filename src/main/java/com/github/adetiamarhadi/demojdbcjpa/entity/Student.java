ca8960fed6bfa8dc17f1bf56404ce3c5b38d701f
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-22 09:50:41 +0700

    jpa and hibernate annotations table

---
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Student.java
@@ -0,0 +1,26 @@
+package com.github.adetiamarhadi.demojdbcjpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pelajar")
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+}
