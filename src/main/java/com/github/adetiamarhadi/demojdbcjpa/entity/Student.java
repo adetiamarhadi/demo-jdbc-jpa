71e28ceee696fdcca9183f89103e19a9b8fb4fcb
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-09-22 09:58:12 +0700

    jpa and hibernate annotations column

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Student.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Student.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -22,5 +19,6 @@ public class Student {
     @GeneratedValue
     private int id;
 
+    @Column(name = "nama", nullable = false)
     private String name;
 }
