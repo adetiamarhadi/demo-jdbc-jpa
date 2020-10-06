5691d53db209cee7ed24fb5be67c47f70e0dd739
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-06 10:09:48 +0700

    ManyToMany mapping - customizing the join table

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Student.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Student.java
@@ -27,6 +27,8 @@ public class Student {
     private Passport passport;
 
     @ManyToMany
+    @JoinTable(name = "STUDENT_COURSE", joinColumns = @JoinColumn(name = "STUDENT_ID"),
+            inverseJoinColumns = @JoinColumn(name = "COURSE_ID"))
     private List<Course> courses = new ArrayList<>();
 
     public void addCourse(Course course) {
