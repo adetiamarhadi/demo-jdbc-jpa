24626e6b46ca0dd6d970e31d28e21d11e00cf9e2
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-06 10:01:55 +0700

    ManyToMany mapping - fixing two join tables problem

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Course.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Course.java
@@ -27,7 +27,7 @@ public class Course {
     @OneToMany(mappedBy = "course")
     private List<Review> reviews = new ArrayList<>();
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "courses")
     private List<Student> students = new ArrayList<>();
 
     public void addReview(Review review) {
