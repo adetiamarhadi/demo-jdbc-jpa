8995e62bed2fedeec24681cdb0525093c5c9f5b8
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-08 15:17:24 +0700

    jpa inheritance hierarchies and mappings - joined

--- src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Employee.java
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @NoArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Employee {
 
     @Id
