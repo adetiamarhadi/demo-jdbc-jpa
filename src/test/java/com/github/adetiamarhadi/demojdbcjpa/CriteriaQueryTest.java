148544c31861ccffa628eb5d0d060df1c4bbb969
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-21 13:06:01 +0700

    criteria query - using joins

--- src/test/java/com/github/adetiamarhadi/demojdbcjpa/CriteriaQueryTest.java
+++ src/test/java/com/github/adetiamarhadi/demojdbcjpa/CriteriaQueryTest.java
@@ -8,10 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 @SpringBootTest
@@ -68,6 +65,42 @@ public class CriteriaQueryTest {
         Predicate students = criteriaBuilder.isEmpty(from.get("students"));
 
         query.where(students);
+
+        TypedQuery<Course> typedQuery = this.entityManager.createQuery(query.select(from));
+
+        List<Course> resultList = typedQuery.getResultList();
+
+        resultList.stream().map(course -> course.getName()).forEach(System.out::println);
+    }
+
+    @Test
+    public void innerJoinTest() {
+
+        CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Course> query = criteriaBuilder.createQuery(Course.class);
+
+        Root<Course> from = query.from(Course.class);
+
+        from.join("students");
+
+        TypedQuery<Course> typedQuery = this.entityManager.createQuery(query.select(from));
+
+        List<Course> resultList = typedQuery.getResultList();
+
+        resultList.stream().map(course -> course.getName()).forEach(System.out::println);
+    }
+
+    @Test
+    public void leftJoinTest() {
+
+        CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Course> query = criteriaBuilder.createQuery(Course.class);
+
+        Root<Course> from = query.from(Course.class);
+
+        from.join("students", JoinType.LEFT);
 
         TypedQuery<Course> typedQuery = this.entityManager.createQuery(query.select(from));
 
