a166b5e4202245c03e854520ccf9bdf8b0f5c82a
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-26 11:06:35 +0700

    introduction to spring data jpa

---
+++ src/main/java/com/github/adetiamarhadi/demojdbcjpa/entity/CourseSpringDataRepository.java
@@ -0,0 +1,6 @@
+package com.github.adetiamarhadi.demojdbcjpa.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
+}
