d36d15ab05c3fab3eabd781bf40c57a8f734da12
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-10-26 11:50:22 +0700

    spring data jpa repository - crud methods

--- src/test/java/com/github/adetiamarhadi/demojdbcjpa/jpa/CourseSpringDataRepositoryTest.java
+++ src/test/java/com/github/adetiamarhadi/demojdbcjpa/jpa/CourseSpringDataRepositoryTest.java
@@ -4,7 +4,9 @@ import com.github.adetiamarhadi.demojdbcjpa.entity.Course;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -26,5 +28,26 @@ public class CourseSpringDataRepositoryTest {
     public void testFindByIdCourseNotPresent() {
         Optional<Course> course = this.repository.findById(2222L);
         assertFalse(course.isPresent());
+    }
+
+    @Test
+    @DirtiesContext
+    public void testInsertAndUpdate() {
+        Course course = Course.builder()
+                .name("Ansible in 100 Steps")
+                .build();
+        this.repository.save(course);
+
+        course.setName("Ansible in 100 Steps - Updated");
+        this.repository.save(course);
+    }
+
+    @Test
+    public void testSelectAndCount() {
+        List<Course> courses = this.repository.findAll();
+        long count = this.repository.count();
+        System.out.println("all courses");
+        courses.stream().map(course -> course.getName()).forEach(System.out::println);
+        System.out.println("count : " + count);
     }
 }
